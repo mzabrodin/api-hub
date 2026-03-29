654c5382b5ee6f6a1a6fe468149811ef385a1120
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-03-29 21:16:48 +0300

    add request type to auth and category controllers

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -2,18 +2,27 @@ import type {Request, Response} from "express";
 import {catchAsync} from "../utils/errors/catchAsync.js";
 import * as AuthService from "../services/auth.service.js";
 import {sendCreated, sendNoContent, sendSuccess} from "../utils/response.js";
+import type {
+    RegisterRequest,
+    LoginRequest,
+    RefreshTokenRequest,
+    VerifyEmailRequest,
+    ForgotPasswordRequest,
+    ResetPasswordRequest,
+} from "../schemas/auth.schemas.js";
+import type {ChangePasswordRequest} from "../schemas/user.schemas.js";
 
-export const register = catchAsync(async (req: Request, res: Response) => {
+export const register = catchAsync(async (req: Request<{}, {}, RegisterRequest>, res: Response) => {
     const user = await AuthService.register(req.body);
     sendCreated(res, {user});
 });
 
-export const verifyEmail = catchAsync(async (req: Request, res: Response) => {
+export const verifyEmail = catchAsync(async (req: Request<{}, {}, VerifyEmailRequest>, res: Response) => {
     await AuthService.verifyEmail(req.body.token);
     sendNoContent(res);
 });
 
-export const login = catchAsync(async (req: Request, res: Response) => {
+export const login = catchAsync(async (req: Request<{}, {}, LoginRequest>, res: Response) => {
     const result = await AuthService.login(req.body);
     sendSuccess(res, result);
 });
@@ -23,22 +32,22 @@ export const logout = catchAsync(async (req: Request, res: Response) => {
     sendNoContent(res);
 });
 
-export const refresh = catchAsync(async (req: Request, res: Response) => {
+export const refresh = catchAsync(async (req: Request<{}, {}, RefreshTokenRequest>, res: Response) => {
     const result = await AuthService.refresh(req.body.refreshToken);
     sendSuccess(res, result);
 });
 
-export const forgotPassword = catchAsync(async (req: Request, res: Response) => {
+export const forgotPassword = catchAsync(async (req: Request<{}, {}, ForgotPasswordRequest>, res: Response) => {
     await AuthService.forgotPassword(req.body.email);
     sendNoContent(res);
 });
 
-export const resetPassword = catchAsync(async (req: Request, res: Response) => {
+export const resetPassword = catchAsync(async (req: Request<{}, {}, ResetPasswordRequest>, res: Response) => {
     await AuthService.resetPassword(req.body.token, req.body.password);
     sendNoContent(res);
 });
 
-export const changePassword = catchAsync(async (req: Request, res: Response) => {
+export const changePassword = catchAsync(async (req: Request<{}, {}, ChangePasswordRequest>, res: Response) => {
     await AuthService.changePassword(req.user!.id, req.body);
     sendNoContent(res);
 });
--- server/src/controllers/category.controller.ts
+++ server/src/controllers/category.controller.ts
@@ -2,7 +2,8 @@ import type {Request, Response} from "express";
 import {catchAsync} from "../utils/errors/catchAsync.js";
 import * as CategoryService from "../services/category.service.js";
 import {sendCreated, sendNoContent, sendSuccess} from "../utils/response.js";
-import IdParam from "../types/idParam.js";
+import type IdParam from "../types/idParam.js";
+import type {CreateCategoryRequest, UpdateCategoryRequest} from "../schemas/category.schemas.js";
 
 export const getAll = catchAsync(async (_req: Request, res: Response) => {
     const categories = await CategoryService.getAll();
@@ -14,12 +15,12 @@ export const getById = catchAsync(async (req: Request<IdParam>, res: Response) =
     sendSuccess(res, {category});
 });
 
-export const create = catchAsync(async (req: Request, res: Response) => {
+export const create = catchAsync(async (req: Request<{}, {}, CreateCategoryRequest>, res: Response) => {
     const category = await CategoryService.create(req.body);
     sendCreated(res, {category});
 });
 
-export const update = catchAsync(async (req: Request<IdParam>, res: Response) => {
+export const update = catchAsync(async (req: Request<IdParam, {}, UpdateCategoryRequest>, res: Response) => {
     const category = await CategoryService.update(req.params.id, req.body);
     sendSuccess(res, {category});
 });
