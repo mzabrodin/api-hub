11bfd7bae93227bc10d295d8e68e4e2212b2624f
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-03-29 05:03:42 +0300

    add JWT type definition and extend Express request object with user property

---
+++ server/src/types/jwt.ts
@@ -0,0 +1,18 @@
+import type { JwtPayload } from "jsonwebtoken";
+import {UserRole} from "../prisma/generated/enums.js";
+
+interface JWT extends JwtPayload {
+    id: string;
+    email: string;
+    role: UserRole;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: JWT;
+        }
+    }
+}
+
+export default JWT;
