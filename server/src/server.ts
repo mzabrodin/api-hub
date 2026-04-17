0831aa037a731a6869546f2bd91e32dc76d89552
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-04-17 22:40:01 +0300

    refactor server setup to include graceful shutdown

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,16 @@
-import "dotenv/config";
-import app from "./app.js"
+import app from "./app.js";
 import CONFIG from "./config.js";
+import prisma from "./prisma/client.js";
 
-const port = CONFIG.port;
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+const server = app.listen(CONFIG.port, () => {
+    console.log(`Listening on port ${CONFIG.port}`);
 });
+
+async function shutdown() {
+    server.close(async () => {
+        await prisma.$disconnect();
+    });
+}
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
